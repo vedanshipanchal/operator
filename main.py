b082444e4c6fb4a470db86319e8d4751b1e61e80
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-21 17:24:33 +0100

    operator

---
+++ main.py
@@ -0,0 +1,15 @@
+#1 part
+a = 5
+b = 10
+c = 15
+d = 20
+e = 25
+res = a**5+(c+d-e)/b   #expression
+print("The result is",res)
+#2 part
+name = "Vedanshi" 
+age = 13
+if name == "Vedanshi":
+    print("hello, how are you")
+else:
+    print("bye, see you soon")
